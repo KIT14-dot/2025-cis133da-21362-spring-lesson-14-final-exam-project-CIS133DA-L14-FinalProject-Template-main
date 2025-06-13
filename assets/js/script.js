b185b5cdffcec2c1c677ad47301a28b24769c901
KIT14-dot <KIT2117694@maricopa.edu>
2025-06-13 17:20:47 -0400

    index

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// Mobile navigation toggle
+function toggleNav() {
+  const nav = document.querySelector('.nav-menu');
+  nav.classList.toggle('open');
+}
+
+// Example usage: could be called on a button click
+// document.querySelector('#nav-toggle').addEventListener('click', toggleNav);
